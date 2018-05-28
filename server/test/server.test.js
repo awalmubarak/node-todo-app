c366e6cb4d971ccdefeb30a7825c27f853d45693
awalmubarak <awalmubarak6@gmail.com>
2018-05-28 12:56:12 +0000

    Add test for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,12 @@ const expect = require('expect');
 const {Todo} = require('./../models/todo');
 const {app} = require('./../server');
 
+const todos = [{title: 'test todo one'}, {title:'test todo two'}];
 
 beforeEach((done)=>{
-  Todo.remove({}).then(()=>done());
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);
+  }).then(()=>done());
 });
 
 
@@ -27,7 +30,7 @@ describe('Server', ()=>{
           if (err) {
             return done(err);
           }
-          Todo.find().then((todos)=>{
+          Todo.find({title}).then((todos)=>{
             expect(todos.length).toBe(1);
             expect(todos[0].title).toBe(title);
             done();
@@ -45,11 +48,21 @@ describe('Server', ()=>{
             return done(err);
           }
           Todo.find().then((todos)=>{
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           }).catch((e)=>done(e));
         });
     });
 
+  });
+
+  describe('GET /todos', ()=>{
+    it('should get all todos', (done)=>{
+      request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=>expect(res.body.length).toBe(2))
+        .end(done());
+    });
   });
 });

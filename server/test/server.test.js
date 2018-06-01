b005bce4ba15441ea6831b36d84dfc8c0be4a509
awalmubarak <awalmubarak6@gmail.com>
2018-06-01 16:54:54 +0000

    Add test for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -286,4 +286,25 @@ describe('Server', ()=>{
   });
 
 
+  describe('DELETE /users/me/token', ()=>{
+    it('should delete token when logged out', (done)=>{
+      request(app)
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err, res)=>{
+          if(err){
+            return done(err);
+          }
+
+          User.findById(users[0]._id).then((user)=>{
+            expect(user).toExist();
+            expect(user.tokens.length).toBe(0);
+            done();
+          }).catch((e)=>done(e));
+        })
+    });
+  });
+
+
 });

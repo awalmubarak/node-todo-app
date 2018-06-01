c2da6c44435235b5e157dad25170acd703931e92
awalmubarak <awalmubarak6@gmail.com>
2018-06-01 15:45:59 +0000

    Add POST /users/login route

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -4,12 +4,12 @@ var authenticate = (req, res, next) => {
     var token = req.header('x-auth');
     User.findByToken(token).then((user) => {
         if (!user) {
-            return Promise.reject();
+            return Promise.reject('Not found');
         }
         req.user = user;
         req.token = token;
         next();
-    }).catch((err) => res.status(401).send());
+    }).catch((err) => res.status(401).send(err));
 }
 
 module.exports = {authenticate};
